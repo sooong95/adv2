3593fd3223e546c0e223d03d4602e20eecc2decb
song <sooong137@gmail.com>
2025-03-20 15:28:53 +0900

    chatting program

---
+++ src/chat/server/ServerMain.java
@@ -0,0 +1,19 @@
+package chat.server;
+
+import java.io.IOException;
+
+public class ServerMain {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+
+        SessionManager sessionManager = new SessionManager();
+
+        // CommandManager 점진적으로 변경
+        CommandManagerV2 commandManager = new CommandManagerV2(sessionManager);
+
+        Server server = new Server(PORT, commandManager, sessionManager);
+        server.start();
+    }
+}
